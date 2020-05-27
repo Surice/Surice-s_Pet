cdd418e479e8c3485f89a89f5174cb5d3f9389b3
Sebastian Ulrich <sesi.ulrich@gmail.com>
2020-05-27 22:54:42 +0200

    dc assistent monitoring added

---
+++ src/automatic/monitoring.js
@@ -0,0 +1,24 @@
+module.exports = async (client, fs, cron, mail, mailOptions)=> {
+    const config = JSON.parse(fs.readFileSync(`${__dirname}/../config.json`,'utf-8'));
+
+    let user1 = await client.users.fetch(config.owner),
+        user2 = await client.users.fetch(config.owner2);
+
+    var offMsgSend = true;
+
+    var j = cron.job('0 */5 * * * *', function(){
+        if(offMsgSend && user1.presence.clientStatus.web != "online" || offMsgSend && user2.presence.clientStatus.web != "online"){
+            offMsgSend = false;
+            console.log("!Monitoring Alert!");
+            console.log(user1.presence.clientStatus);
+            console.log(user2.presence.clientStatus);
+
+            mailOptions.subject = "Your Discord Assistent may Went Offline!";
+            mailOptions.text =`the monitoring system has reported an error. you should check the server status. \nno further errors information \n\n\non behalf of sebastian ulrich \n\nthis is a machine generated letter. an answer to this email cannot be handled. in case of problems or questions please write to info@sebastian-web.de`;
+            mail.sendMail(mailOptions);
+        }else{
+            offMsgSend = true;
+        }
+    });
+    j.start();
+}
--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 const Discord = require('discord.js');
 const fs = require("fs");
+const nodemailer = require('nodemailer');
+const cron = require('cron');
 
 const config = JSON.parse(fs.readFileSync(`${__dirname}/config.json`,'utf-8'));
 
@@ -7,11 +9,27 @@ var users = JSON.parse(fs.readFileSync(`${__dirname}/stor/users.json`).toString(
 var channels = JSON.parse(fs.readFileSync(`${__dirname}/stor/channels.json`).toString());
 
 var global = require(`${__dirname}/global.js`);
+
 var client = new Discord.Client(),
     myuser = global.myuser,
     unick = global.unick,
     save = new Array();
 
+var mail = nodemailer.createTransport({
+    host: 'smtp.strato.de',
+	port: 465,
+	secure: true,
+    auth: {
+        user: config.mailUser,
+        pass: config.mailPass
+    }
+    }),
+    mailOptions = {
+        from: 'Bot-Status@sebastian-web.de',
+	    to: 'sesi.ulrich@gmail.com',
+	    subject: '',
+	    text: ''
+    };
 
 console.log("Loading...");
 client.on('ready', ()=>{
@@ -21,6 +39,9 @@ client.on('ready', ()=>{
 
     client.user.setStatus("idle");
     client.user.setActivity("for Preifx: $$$", {type: "WATCHING"});
+
+    run = require(`${__dirname}/automatic/monitoring`);
+    run(client, fs, cron, mail, mailOptions);
 });
 
 client.on('guildMemberUpdate', function (guild, oldguild){
@@ -47,7 +68,6 @@ client.on('message', (msg)=>{
                     let filerun = require(`${__dirname}/modules/${content[0]}.js`)
                     filerun(client, msg, content, authorized(msg));
                 }catch(err){
-                    console.log(err);
                     if(authorized(msg).includes("admin")){
                         try{
                             console.log("try run admin command");
@@ -61,7 +81,8 @@ client.on('message', (msg)=>{
                                     filerun(client, msg, content, authorized(msg));
                                 }catch(err){             
                                     //if no owner command found
-                                    if(err.code == "MODULE_NOT_FOUND"){
+                                    if(err.code == 'MODULE_NOT_FOUND'){
+                                        console.log("cant find this command");
                                         msg.channel.send(`the entered command doesn't exist.`);
                                     }else{
                                         console.log(err);
@@ -69,25 +90,21 @@ client.on('message', (msg)=>{
                                 }
                             }else{
                                 //if not owner authorized and no admin command found
+                                if(err.code != 'MODULE_NOT_FOUND'){
+                                    console.log(err);
+                                }
                                 msg.channel.send(`the entered command doesn't exist or you aren´t authorized to give me this order. Please try again or contact <@${config.owner}> in case of a mistake`);
                             }
                         }
                     }else{
                         //if not admin authorized and no command found
+                        if(err.code != "MODULE_NOT_FOUND"){
+                            console.log(err);
+                        }
                         msg.channel.send(`the entered command doesn't exist or you aren´t authorized to give me this order. Please try again or contact <@${config.owner}> in case of a mistake`);
                     }
                 }
             }
-/*
-            if(errorLog){
-                if(errorLog.code == 'MODULE_NOT_FOUND'){
-                    console.log("Wrong input. no command found");
-                    msg.channel.send("the entered command doesn't exist");
-                }else{
-                    console.log(errorLog);
-                }
-            }
-*/
         }
     }
 });
@@ -116,13 +133,20 @@ function authorized(msg){
 
 
 try{
-    client.login(config.token)
+    client.login(config.token);
 }catch(err){
     console.log(err);
+    mailOptions.subject = "an error was detected";
+    mailOptions.text =  `Unknown Error \n\nERROR: \n${err}`;
+
     if(err.code == 'EAI_AGAIN' || err.code == 'ETIMEDOUT'){
+        mailOptions.text = `an Connection Timeout was detected! \n\nERROR: \n${err}`;
         console.log(`------------------------\n>CONNECTION ERROR DETECTED<\n------------------------`);
         setTimeout(function(){
             client.login(config.token)
         }, 30000);
     }
+    mail.sendMail(mailOptions, function(error, info){
+        console.log(info);
+    });
 }
