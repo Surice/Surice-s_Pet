c3f2f6f7fe6febd76f78164a8b034a82b494d1db
Sebastian Ulrich <sesi.ulrich@gmail.com>
2020-08-13 18:59:51 +0200

    some purge fixes

--- src/modulesAdmin/purge.js
+++ src/modulesAdmin/purge.js
@@ -1,14 +1,22 @@
 module.exports = async (client, msg, content) => {
+    var err = false;
+
     let amount = content[1] || 1;
     amount++;
 
-    msg.channel.bulkDelete(amount).catch(function(error){
+    await msg.channel.bulkDelete(amount).catch(function(error){
+        err = true;
         if(error.code == "50034"){
-            msg.channel.send("It is possible to delete these old messages. They must be less than 14 days old");
+            msg.channel.send("It isn´t possible to delete these old messages. They must be less than 14 days old");
         }
         else if(error.code == "50035"){
             msg.channel.send("Can´t delete more than __100__ messages. please choose a value less than 100");
         }
+        else{
+            msg.channel.send("an unexpected error has occurred");
+        }
     });
-    msg.channel.send(`${amount - 1} Messages Deleted✅`).then(message => setTimeout(function(){message.delete()}, 2000));
+    if(!err){
+        msg.channel.send(`${amount - 1} Messages Deleted✅`).then(message => setTimeout(function(){message.delete()}, 2000));
+    }
 }
