c0739e8d1d8dd36b38d048e3855eeb672d0d8ebf
Sebastian Ulrich <sesi.ulrich@gmail.com>
2020-11-01 20:43:19 +0100

    checkChannel handle user completed

--- src/modulesOwner/checkChannel.js
+++ src/modulesOwner/checkChannel.js
@@ -6,9 +6,6 @@ module.exports = async (client, msg, content, placeholder, check, oldMember, new
 
     if(check){
         oldMember.member.send("du bist in der Warteschlange. Bitte hab einen moment gedult");
-        console.log(oldMember.id);
-        console.log(oldMember.member.id);
-        console.log(oldMember.member.user.id);
 
         var me = await client.users.fetch(config.owner);
         me.send(`<@${oldMember.id}> wartet um gemovet zu werden!`).then(e =>{
@@ -24,22 +21,22 @@ module.exports = async (client, msg, content, placeholder, check, oldMember, new
     }else if(newMember && react){
         if(newMember.id == config.owner){
             var content = react.message.content.split(' ');
-            console.log(react.message.content);
-            var temp = content[0].substr(3).slice(0, -1);
+            var temp = content[0].substr(2).slice(0, -1);
             content[0] = temp;
 
             const chan = await client.channels.cache.get(global.targetId);
+            const wChan = await client.channels.cache.get(global.channelId);
             const guild = chan.guild
-            const member = await guild.members.fetch(content[0]);
+            const member = guild.members.cache.get(content[0]);
 
             if(react.emoji.name == '✅'){                
                 member.voice.setChannel(chan);
             }else if(react.emoji.name == '❌'){
                 member.voice.kick();
 
-                chan.createOverwrite(member, {
+                wChan.createOverwrite(member, {
                     'CONNECT_CHANNEL': false
-                })
+                });
             }
             react.message.delete();
         }
