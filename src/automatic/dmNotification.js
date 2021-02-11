6b9e6aa53c577368dc06f39a4bbf32d84a5f88af
Sebastian Ulrich <sesi.ulrich@gmail.com>
2021-02-11 16:35:01 +0100

    dmNotification reinforced

--- src/automatic/dmNotification.js
+++ src/automatic/dmNotification.js
@@ -2,12 +2,12 @@ module.exports = (client, process, user, mod, reason, server, time) => {
     const Discord = require('discord.js');
 
     var embed = new Discord.MessageEmbed()
-    .setTitle(process)
+    .setTitle(`${process} ${server.name}`)
     .setThumbnail(server.iconURL())
     .setColor('0xc22a1f')
     .setAuthor(mod.tag, mod.avatarURL())
-    .setDescription(`you were ${process} ${server.name}`)
     .addField("Reason:", reason)
+    .addField("Enforced by:", `<@${mod.id}>`)
     .setTimestamp(new Date());
 
     if(time){
